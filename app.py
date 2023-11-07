e1c55d849f0a0ab4ad66c082b065ca01a3691c45
Gilad Alboher <gilad1299@gmail.com>
2023-11-07 19:42:49 +0200

    fix the problem with the dogecoin and deleted the dogecoin lines

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, session, redirect
 from functools import wraps
 import pymongo
 import requests
-from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+
 
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
@@ -54,13 +54,15 @@ def market():
   btcusdper = float(str(html_response.text).split('Bitcoin USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
   ethusd = str(html_response.text).split('Ethereum USD')[1].split('realtime">')[1].split('<')[0]
   ethusdper = float(str(html_response.text).split('Ethereum USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
-  dogeusd = str(html_response.text).split('Dogecoin USD')[1].split('realtime">')[1].split('<')[0]
-  dogeusdper = float(str(html_response.text).split('Dogecoin USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
+  # dogeusd = str(html_response.text).split('Dogecoin USD')[1].split('realtime">')[1].split('<')[0]
+  # dogeusdper = float(str(html_response.text).split('Dogecoin USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
   xrpusd = str(html_response.text).split('XRP USD')[1].split('realtime">')[1].split('<')[0]
   xrpusdper = float(str(html_response.text).split('XRP USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
   ltcusd = str(html_response.text).split('Litecoin USD')[1].split('realtime">')[1].split('<')[0]
   ltcusdper = float(str(html_response.text).split('Litecoin USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
   bchusd = str(html_response.text).split('Bitcoin Cash USD')[1].split('realtime">')[1].split('<')[0]
   bchusdper = float(str(html_response.text).split('Bitcoin Cash USD')[1].split('percentchange')[1].split('</bg-quote>')[0].split('>')[1].replace('%', ''))
-  return render_template('market.html' , btc = btcusd , btcper = btcusdper , eth = ethusd , ethper = ethusdper , doge = dogeusd , dogeper = dogeusdper , xrp = xrpusd , xrpper = xrpusdper , ltc = ltcusd , ltcper = ltcusdper , bch = bchusd , bchper = bchusdper)
 
+  # return render_template('market.html' , btc = btcusd , btcper = btcusdper , eth = ethusd , ethper = ethusdper , doge = dogeusd , dogeper = dogeusdper , xrp = xrpusd , xrpper = xrpusdper , ltc = ltcusd , ltcper = ltcusdper , bch = bchusd , bchper = bchusdper)
+  return render_template('market.html' , btc = btcusd , btcper = btcusdper , eth = ethusd , ethper = ethusdper , xrp = xrpusd , xrpper = xrpusdper , ltc = ltcusd , ltcper = ltcusdper , bch = bchusd , bchper = bchusdper)
+  
